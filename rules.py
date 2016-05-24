fdd8021ef425284daf5b62476ec5e6a384e1217f
VadimKovalchuk <kovalchuk.vadim@gmail.com>
2016-05-24 17:58:33 +0300

    Minor updates in rules

--- rules.py
+++ rules.py
@@ -19,38 +19,22 @@ class Rules:
         Performs day phase actions.
         '''
         result = {player.id: {} for player in self.session.players}
-        for location in self.session.map:
-            if location.type == 'standard':
-                for man in location.slots:
-                    if not man:
-                        continue
-                    if location.name not in result[man.player]:
-                        result[man.player][location.name] = {}
-                    dice_result = randint(1,man.points)
-                    result[man.player][location.name][man.name] = dice_result
-                    #man.is_allocated = False
-
+        locations = []
+        locations.extend(self.session.map)
         for player in self.session.players:
-            for farm in player.farm:
-                for man in farm.slots:
-                    if not man:
-                        continue
-                    if farm.name not in result[man.player]:
-                        result[player.id][farm.name] = {}
-                    dice_result = randint(1,man.points)
-                    result[man.player][farm.name][man.name] = dice_result
-                    #man.is_allocated = False
+            locations.extend(player.infra)
+            locations.extend(player.farm)
 
-        for player in self.session.players:
-            for building in player.infa:
-                for man in building.slots:
-                    if not man:
-                        continue
-                    if building.name not in result[man.player]:
-                        result[player.id][building.name] = {}
-                    dice_result = randint(1,man.points)
-                    result[man.player][building.name][man.name] = dice_result
-                    #man.is_allocated = False
+        for location in locations:
+            for man in location.slots:
+                if not man:
+                    continue
+                if location.name not in result[man.player]:
+                    result[man.player][location.name] = {}
+                dice_result = randint(1,man.points)
+                result[man.player][location.name][man.name] = dice_result
+                #man.is_allocated = False
+
 
 
         return result
